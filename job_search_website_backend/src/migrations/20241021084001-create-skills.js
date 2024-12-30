fd8be7d131237e072450b72e70345277c8c821bd
Tam-Vu <vuthanhtam12062003@gmail.com>
2024-12-30 14:56:29 +0700

    a

--- job_search_website_backend/src/migrations/20241021084001-create-skills.js
+++ job_search_website_backend/src/migrations/20241021084001-create-skills.js
@@ -9,7 +9,10 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      name: {
+      value: {
+        type: Sequelize.INTEGER,
+      },
+      label: {
         type: Sequelize.STRING,
       },
       createdAt: {
--- job_search_website_backend/src/server.js
+++ job_search_website_backend/src/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173",
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
     methods: "GET,POST,PUT,PATCH,DELETE",
     credentials: true,
   })
