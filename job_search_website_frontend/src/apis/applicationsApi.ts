cc8ae479c0a700285dc3c2ea2bc572f87425d05a
EdogawaCoTrung <quangtrungproki@gmail.com>
2024-12-31 17:28:50 +0700

    add

--- job_search_website_frontend/src/apis/applicationsApi.ts
+++ job_search_website_frontend/src/apis/applicationsApi.ts
@@ -1,8 +1,39 @@
 import { httpClient } from "@/services"
 import { Response } from "@/type"
+import { UpdateData } from "./resume"
 
 interface createApplicationRes extends Response {
   DT: ""
+}
+
+interface applicationResume extends UpdateData {
+  employee: {
+    id: number
+    fullName: string
+    userId: number
+    user: {
+      email: string
+      image: string
+    }
+  }
+}
+
+export interface getApplicationByJobIdRes extends Response {
+  DT: {
+    id: number
+    resumeId: number
+    jobId: number
+    status: string
+    resume: applicationResume
+  }[]
+}
+
+export interface getApplicationByJob {
+  id: number
+  resumeId: number
+  jobId: number
+  status: string
+  resume: applicationResume
 }
 
 class ApplicationApi {
@@ -17,6 +48,33 @@ class ApplicationApi {
     // eslint-disable-next-line no-useless-catch
     try {
       const res = await httpClient.post<createApplicationRes>(`/applications/apply/${id}`, { resumeId })
+      return res
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  async getApplicationByJobId(id: number) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      const res = await httpClient.get<getApplicationByJobIdRes>(`/applications/${id}`)
+      return res
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  async approveApplication(id: number) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      const res = await httpClient.patch<createApplicationRes>(`/applications/approve/${id}`)
+      return res
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  async rejectApplication(id: number) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      const res = await httpClient.patch<createApplicationRes>(`/applications/reject/${id}`)
       return res
     } catch (error) {
       console.log(error)
--- job_search_website_frontend/src/components/shared/ui/BackgroundBeam.tsx
+++ job_search_website_frontend/src/components/shared/ui/BackgroundBeam.tsx
@@ -87,7 +87,7 @@ export const BackgroundBeams = React.memo(({ className }: { className?: string }
           ></motion.path>
         ))}
         <defs>
-          {paths.map((path, index) => (
+          {paths.map((_path, index) => (
             <motion.linearGradient
               id={`linearGradient-${index}`}
               key={`gradient-${index}`}
